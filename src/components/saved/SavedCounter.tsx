59fc4e1e57556e802047a21d92734a06bb8699e5
Jonas Wanner <jonas.wanner@clavisit.com>
2023-06-19 09:54:29 +0200

    feat: adding counter hook to saved counter

--- src/components/saved/SavedCounter.tsx
+++ src/components/saved/SavedCounter.tsx
@@ -6,18 +6,20 @@ import dayjs from "dayjs"
 import RelativeTime from "@/components/basic/RelativeTime";
 import Link from "next/link";
 import React from 'react'
+import {useCounter} from "@/hooks/counter";
 
-export default function SavedCounter({title, id, count}: Counter) {
+export default function SavedCounter(init: Counter) {
+    const { counter } = useCounter(init)
+
     return (
-        <Link href={`/counter/${id}`} className="">
+        <Link href={`/counter/${counter.id}`} className="">
             <div className="w-full lg:min-w-[300px] lg:max-w-[400px] rounded-xl grid grid-cols-saved-counter gap-4 p-6 bg-gray-500/10 border border-gray-300/10">
                 <div className="grid place-items-center">
-                    <p className="font-bold text-[20px]"><ShortNumber number={count}/>
-                    </p>
+                    <p className="font-bold text-[20px]"><ShortNumber number={counter.count}/></p>
                 </div>
                 <div className="flex flex-col">
-                    <h3 className="text-xl font-bold truncate mb-[2px]">{title}</h3>
-                    <span className="text-gray-600 text-sm italic">updated <RelativeTime date={dayjs().toISOString()}/></span>
+                    <h3 className="text-xl font-bold truncate mb-[2px]">{counter.title}</h3>
+                    <span className="text-gray-600 text-sm italic">updated <RelativeTime date={dayjs(counter.modified)}/></span>
                 </div>
                 <div>
                 </div>

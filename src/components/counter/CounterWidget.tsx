a34ae2c2d83d59e70222d6d164edd7cbdb36f5f1
Jonas Wanner <jonas@wanner.com>
2023-06-20 08:27:54 +0200

    feat: improving saved elements

--- src/components/counter/CounterWidget.tsx
+++ src/components/counter/CounterWidget.tsx
@@ -5,17 +5,17 @@ import ShortNumber from "@/components/basic/ShortNumber"
 import dayjs from "dayjs"
 import RelativeTime from "@/components/basic/RelativeTime";
 import Link from "next/link";
-import React, {useEffect} from 'react'
+import React from 'react'
 import {useCounter} from "@/hooks/counter";
 
-export default function SavedCounter(init: Counter) {
+export default function CounterWidget(init: Counter) {
     const { counter } = useCounter(init)
 
     return (
         <Link href={`/counter/${counter.id}`} className="">
             <div className="w-full lg:min-w-[300px] lg:max-w-[400px] rounded-xl grid grid-cols-saved-counter gap-4 p-6 bg-gray-500/10 border border-gray-300/10">
                 <div className="grid place-items-center">
-                    <p className="font-bold text-[20px]"><ShortNumber number={counter.count}/></p>
+                    <p className="font-bold text-[20px]"><ShortNumber key={counter.count} number={counter.count}/></p>
                 </div>
                 <div className="flex flex-col">
                     <h3 className="text-xl font-bold truncate mb-[2px]">{counter.title}</h3>
